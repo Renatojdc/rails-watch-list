978038fd4c2856ba739b5b31ec8df369b818644f
Renato Jardim Di Carlantonio <renatojdc@outlook.com>
2022-08-18 16:42:01 -0300

    bookmark set up expanding

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,10 +1,24 @@
 class BookmarksController < ApplicationController
   def new
+    @bookmark = Bookmark.new
   end
 
   def create
+    @bookmark = Bookmark.new(bookmark_params)
+
+    if @bookmark.save
+      redirect_to list_path, notice: 'Bookmark was successfully added.'
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment)
   end
 end
