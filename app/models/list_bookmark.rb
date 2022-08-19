6ff5815f6131c9232b1459e3021fd4353ee54bdf
Renato Jardim Di Carlantonio <renatojdc@outlook.com>
2022-08-19 16:42:25 -0300

    bookmarks done

--- app/models/list_bookmark.rb
+++
@@ -1,4 +0,0 @@
-class ListBookmark < ApplicationRecord
-  belongs_to :list
-  belongs_to :bookmark
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
-  get 'bookmarks/destroy'
-  get 'lists/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
